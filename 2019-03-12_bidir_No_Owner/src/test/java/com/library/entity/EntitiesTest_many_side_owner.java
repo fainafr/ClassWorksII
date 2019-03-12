188ddf10d56c671e33dead5fce92ac4c9328ab1b
Philipp Vitkovsky <philipp.vitkovsky@gmail.com>
2019-03-12 14:35:50 +0200

    Preparation for bidir three types of owning entity

---
+++ 2019-03-12_bidir_No_Owner/src/test/java/com/library/entity/EntitiesTest_many_side_owner.java
@@ -0,0 +1,82 @@
+package com.library.entity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.library.repo.IEventRepo;
+import com.library.repo.IUserRepo;
+
+/**
+ * Testing creation and deletion of entities to ensure relations work as
+ * intended;
+ * 
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@Transactional
+public class EntitiesTest_many_side_owner {
+
+	private final String ANAME = "Alyssa";
+	private final String TNAME = "Testing"; 
+	private final User ALYSSA = new User();
+	private final Event TESTING = new Event();
+
+	@Autowired
+	IUserRepo userRepo;
+	@Autowired
+	IEventRepo eventRepo;
+
+	@Before
+	public void build() {
+		
+		ALYSSA.setUserName(ANAME);
+		TESTING.setName(TNAME);
+		TESTING.setUser(ALYSSA);
+
+	}
+
+	@Test
+	public void saveRead() {
+
+		eventRepo.save(TESTING);
+
+		Event savedE = eventRepo.findAll().get(0); 
+		User savedA = userRepo.findAll().get(0);
+		User savedAfromE = savedE.getUser();
+		
+		assertEquals(savedE, TESTING);
+		assertEquals(savedA, ALYSSA);
+		assertEquals(savedAfromE, ALYSSA);
+		
+
+	}
+	
+	/**
+	 * Testing no cascading from parent in one-to-many unidir where child is owner;
+	 */
+	@Test
+	public void noCascading() {
+
+		userRepo.save(ALYSSA);
+
+		assertEquals(eventRepo.count(), 0);
+		assertEquals(userRepo.count(), 1);
+		
+		
+
+	}
+
+}
